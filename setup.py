c8e4431270d7a439f323609869cf8ee03141cc4d
Chris Adams <chris@improbable.org>
2010-09-10 15:10:05 -0700

    Rudimentary README and setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = "django-performance-tools",
+    version = "0.1",
+    packages = [
+        "performance_tools",
+    ],
+    author = "Eric Holscher, Chris Adams",
+    author_email = "eric@ericholscher.com, chris@improbable.org",
+    description = "Tools for collecting performance data from Django apps",
+    url = "http://github.com/acdha/django-performance-tools",
+)
