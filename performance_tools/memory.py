17cf0423469d4fbf2106c5cd6b08a3d0c690471a
Chris Adams <chris@improbable.org>
2010-09-10 17:35:15 -0700

    Migrated guppy plugin from django-test-utils

---
+++ performance_tools/memory.py
@@ -0,0 +1,19 @@
+"""
+Heap profiling using Guppy
+"""
+
+from guppy import hpy
+
+class Heap(object):
+    """
+    Calculate heap consumed before and after request
+    """
+
+    def __init__(self):
+        self.hp = hpy()
+        self.hp.setrelheap()
+
+    def deltas(self):
+        heap = self.hp.heap()
+        # TODO: Decide what other data we should include - (class, count, size) list?
+        return {"size": heap.size }
